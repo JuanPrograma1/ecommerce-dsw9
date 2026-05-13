1798b4e48366f838585f4398be6cd05785f7be2f
Juan Molina <150199700+JuanPrograma1@users.noreply.github.com>
2026-05-12 19:04:26 -0500

    Ultiman

---
+++ routes/products.js
@@ -0,0 +1,8 @@
+// routes/products.js
+const express = require('express');
+const router  = express.Router();
+const ctrl    = require('../controllers/productController');
+router.get('/',             ctrl.getHomePage);
+router.get('/products',     ctrl.getAllProducts);
+router.get('/products/:id', ctrl.getProductById);
+module.exports = router;
